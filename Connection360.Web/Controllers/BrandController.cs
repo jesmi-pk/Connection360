ec77b00edd0e36e1590e5ca3006fa2e51e4e766a
Noyal <noyalvincy@gmail.com>
2017-02-03 10:26:01 +0530

    [CON360:002.R] Reviewed the code, provided comments in brandcontroller, Layout page, brand index page.

--- Connection360.Web/Controllers/BrandController.cs
+++ Connection360.Web/Controllers/BrandController.cs
@@ -1,12 +1,9 @@
-﻿using Connection360.Web.Models;
-using Connection360.Web.Service;
+﻿using Connection360.Web.Service;
 using Connection360.Web.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using PagedList;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Connection360.Web.Controllers
 {
@@ -14,24 +11,25 @@ namespace Connection360.Web.Controllers
     {
         BrandService brandService = new BrandService();
         SkuService skuService = new SkuService();
-        HomeViewModel homeViewModel = new HomeViewModel();    
-        public ActionResult Index(string brandName, string skuName,int? page)
+        HomeViewModel homeViewModel = new HomeViewModel();
+        public ActionResult Index(string brandName, string skuName, int? page)
         {
             var result = (from b in brandService.brands
                           join s in skuService.skus on b.Id
-                          equals s.Id select new HomeViewModel()
+                          equals s.Id
+                          select new HomeViewModel()
                           {
                               Id = b.Id,
                               Name = b.Name,
                               SkuName = s.Name
                           }).ToList();
-            if (!String.IsNullOrWhiteSpace(brandName) && !String.IsNullOrWhiteSpace(skuName))
-            {               
-               result = result.Where(r => r.Name.Contains(brandName) || r.SkuName.Contains(skuName)).ToList();               
+            if (!String.IsNullOrWhiteSpace(brandName) && !String.IsNullOrWhiteSpace(skuName)) //You dont require two variables, or two searchbox to search two items.
+            {
+                result = result.Where(r => r.Name.Contains(brandName) || r.SkuName.Contains(skuName)).ToList();// search is not working properly, only if two search box have the same value, the value pair, it is showing result
             }
             int pageSize = 3;
             int pageNumber = (page ?? 1);
-            return View(result.ToPagedList(pageNumber,pageSize));
+            return View(result.ToPagedList(pageNumber, pageSize));
         }
     }
 }
